ea4d1619ad71468c796468ebeff666142c9437e3
teerapat <teerapat_12345678@hotmail.com>
2017-06-20 02:21:04 +0700

    react-hot-loader

--- react-hot-demo/src/index.js
+++ react-hot-demo/src/index.js
@@ -3,24 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(
-    <AppContainer>
-      <App />
-    </AppContainer>,
-     document.getElementById('root'));
-if (module.hot) {
-    module.hot.accept('./App',() =>{
-        const NextApp = require('./App').default;
-        ReactDOM.render(
-          <AppContainer>
-                <NextApp />
-          </AppContainer>,
-          document.getElementById('root')
-        );
-    });
-}
-
-
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
+if (module.hot) {
+  module.hot.accept();
+}
