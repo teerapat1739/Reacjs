27a49a87bbbc6a9a00d315233a35d4a216a8dfe7
teerapat <teerapat_12345678@hotmail.com>
2017-06-20 00:57:00 +0700

    react-hot-loader success

--- react-hot-demo/src/App.js
+++ react-hot-demo/src/App.js
@@ -3,21 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      number: 0
-    };
-  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React++</h2>
+          <h2>Welcome to ReactQsxgg</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.!
+          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
--- react-hot-demo/src/index.js
+++ react-hot-demo/src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { AppContainer } from 'react-hot-loader';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
-
-
+ReactDOM.render(
+    <AppContainer>
+      <App />
+    </AppContainer>,
+     document.getElementById('root'));
 if (module.hot) {
-  module.hot.accept();
+    module.hot.accept('./App',() =>{
+        const NextApp = require('./App').default;
+        ReactDOM.render(
+          <AppContainer>
+                <NextApp />
+          </AppContainer>,
+          document.getElementById('root')
+        );
+    });
 }
+
+
+registerServiceWorker();
