2d70fc450ca761df007feac5ce20c802bbbbe3d0
teerapat <teerapat_12345678@hotmail.com>
2017-06-20 14:06:36 +0700

    Add Styled :font img grid

---
+++ react-hot-demo/src/theme/globalStyle.js
@@ -0,0 +1,10 @@
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=VT323');
+
+  body {
+    font-family: 'VT323', monospace;
+    font-size: 24px;
+  }
+`
