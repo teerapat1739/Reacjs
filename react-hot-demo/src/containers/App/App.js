2454ecbfff262587ffaa66bb022e60283e542ebf
teerapat <teerapat_12345678@hotmail.com>
2017-06-30 13:45:30 +0700

    add navigation bar

--- react-hot-demo/src/containers/App/App.js
+++ react-hot-demo/src/containers/App/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import { Background } from './App.style';
 import NavigationBar from '../../components/NavigationBar/NavigationBar';
+import ReallySmoothScroll from 'really-smooth-scroll';
+import { hashHistory } from 'react-router';
+
+hashHistory.listen(() => {
+});
+
+ReallySmoothScroll.shim();
 
 class App extends Component {
   render() {
